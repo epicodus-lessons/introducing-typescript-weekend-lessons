18f6366b91df26500e1b4f9efc657dea731a9d7b
Staff <staff@staff-2.local>
2016-03-15 14:20:05 -0700

    Fix our sum.ts file and remove script tag to old vanilla-sum.js file.

--- app/sum.js
+++ app/sum.js
@@ -1,6 +1,6 @@
 var number;
 var otherNumber;
-number = prompt('please enter a number.');
-otherNumber = prompt('enter another number.');
+number = parseInt(prompt('please enter a number.'));
+otherNumber = parseInt(prompt('enter another number.'));
 var sum = number + otherNumber;
 alert("The sum of your two numbers is: " + sum);
