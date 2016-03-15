c577b70ef4d4e983d1e17551912228b9e982f5fd
Staff <staff@staff-2.local>
2016-03-15 14:14:29 -0700

    Add typescript sum file to show error reporting.

---
+++ app/sum.js
@@ -0,0 +1,6 @@
+var number;
+var otherNumber;
+number = prompt('please enter a number.');
+otherNumber = prompt('enter another number.');
+var sum = number + otherNumber;
+alert("The sum of your two numbers is: " + sum);
---
+++ app/sum.ts
@@ -0,0 +1,7 @@
+var number: number;
+var otherNumber: number;
+
+number = prompt('please enter a number.');
+otherNumber = prompt('enter another number.');
+var sum = number + otherNumber;
+alert("The sum of your two numbers is: " + sum);
